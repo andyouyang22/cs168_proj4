edf16fcc8b332a774a7da1f411926858ea7ea4df
Andy Ouyang <andy@Andys-MacBook-Pro-2.local>
2015-12-02 04:52:54 -0800

    Project 4: Extracted parsing code (needed for Firewall constructor)

--- firewall.py
+++ firewall.py
@@ -12,6 +12,7 @@ from packet import (
 	ICMPHeader,
 	DNSHeader,
 )
+import parse
 import socket
 import struct
 
@@ -28,10 +29,10 @@ class Firewall:
 		self.iface_ext = iface_ext
 
 		# Load the firewall rules (from rule_filename) here.
-		self.rules = self.parse_rules(config['rule'])
+		self.rules = parse.rules(config['rule'])
 
 		# Load the GeoIP DB ('geoipdb.txt') as well.
-		self.geos = self.parse_geos('geoipdb.txt')
+		self.geos = parse.geos('geoipdb.txt')
 
 		# Included so that a mock log file can be stubbed in during testing
 		if 'log' in config:
@@ -39,58 +40,6 @@ class Firewall:
 		else:
 			self.log = open(DEFAULT_LOG, 'w')
 
-	def parse_rules(self, filename):
-		"""
-		Create an array of rules from the rules file specified in the firewall
-		config.
-		"""
-		rules = []
-		rule_file = open(filename)
-		for rule in rule_file:
-			rule = rule.split()
-			if len(rule) < 1:
-				continue
-			for i in range(len(rule)):
-				rule[i] = rule[i].lower()
-			if rule[0] == '%':
-				continue
-			if rule[1] == "tcp" or rule[1] == "udp" or rule[1] == "icmp":
-				new_rule = {
-					'verdict'  : rule[0],
-					'protocol' : rule[1],
-					'ext_ip'   : rule[2],
-					'ext_port' : rule[3],
-				}
-			elif rule[1] == "dns":
-				new_rule = {
-					'verdict'  : rule[0],
-					'protocol' : 'dns',
-					'domain_name' : rule[2],
-				}
-			else:
-				# probably just a line of text, do nothing
-				continue
-			print new_rule
-			rules.append(new_rule)
-		return rules
-
-	def parse_geos(self, filename):
-		"""
-		Create an array of geographical IP mappings from the GeoIP file specified.
-		"""
-		geos = []
-		geo_file = open(filename)
-		for geo_line in geo_file:
-			geo_line = geo_line.split()
-			for i in range(len(geo_line)):
-				geo_line[i] = geo_line[i].lower()
-			new_geo = {
-				'start_ip'     : geo_line[0],
-				'end_ip'       : geo_line[1],
-				'country_code' : geo_line[2],
-			}
-			geos.append(new_geo)
-		return geos
 
 	# @pkt_dir: either PKT_DIR_INCOMING or PKT_DIR_OUTGOING
 	# @pkt: the actual data of the IPv4 packet (including IP header)
